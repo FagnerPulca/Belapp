afd910807189aa07149e03d5be18ee9ffbce1fba
Gleison Alves <gleisonaaraujo@gmail.com>
2018-11-12 14:58:45 -0300

    Criado tela de cadastro básico

---
+++ app/src/main/java/br/com/belapp/belapp/activities/CadastroBasicoActivity.java
@@ -0,0 +1,15 @@
+package br.com.belapp.belapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.belapp.belapp.R;
+
+public class CadastroBasicoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_basico);
+    }
+}
