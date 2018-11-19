9f6860ea0d6fb4e84fdc155ad5c5dea0968e1b97
DanilloMLS <moraesdanillo10@gmail.com>
2018-11-18 22:27:22 -0300

    Inclusão de método para o cálculo da distância entre coordenadas.

--- app/src/main/java/br/com/belapp/belapp/presenter/ApplicationClass.java
+++ app/src/main/java/br/com/belapp/belapp/presenter/ApplicationClass.java
@@ -22,4 +22,20 @@ public class ApplicationClass extends Application {
         testes.add(new Teste("04","Salao Beauty", "Unha"));
         testes.add(new Teste("05","Salao Great", "Olho"));
     }
+
+    private double calculaDistancia(double lat1, double lng1, double lat2, double lng2) {
+        //double earthRadius = 3958.75;//miles
+        double earthRadius = 6371;//kilometers
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLng = Math.toRadians(lng2 - lng1);
+        double sindLat = Math.sin(dLat / 2);
+        double sindLng = Math.sin(dLng / 2);
+        double a = Math.pow(sindLat, 2) + Math.pow(sindLng, 2)
+                * Math.cos(Math.toRadians(lat1))
+                * Math.cos(Math.toRadians(lat2));
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double dist = earthRadius * c;
+
+        return dist * 1000; //em metros
+    }
 }
