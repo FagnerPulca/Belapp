37fc687f3846d4aafe5ba296da9455e50b3263f6
Felipe <flpdias14@gmail.com>
2018-11-21 23:58:06 -0300

    [#161136406] Correção teste de aceitação TA_02. Adição do método getAtualActivity na classe DefaultTest. Correções codacy. Refatoração do teste de busca

--- app/src/androidTest/java/br/com/belapp/belapp/test/BuscaActivitySteps.java
+++ app/src/androidTest/java/br/com/belapp/belapp/test/BuscaActivitySteps.java
@@ -1,7 +1,11 @@
 package br.com.belapp.belapp.test;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.espresso.ViewAssertion;
 import android.support.test.rule.ActivityTestRule;
+import android.view.View;
 
 import org.junit.Rule;
 
@@ -14,68 +18,97 @@ import cucumber.api.java.it.E;
 import cucumber.api.java.it.Quando;
 import cucumber.api.java.pt.Entao;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static junit.framework.TestCase.assertNotNull;
-
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.not;
 
 public class BuscaActivitySteps extends DefaultTest {
 
     @Rule
     public ActivityTestRule<TelaBuscaActivity> activityTestRule = new ActivityTestRule<>(TelaBuscaActivity.class);
-
+    private Activity activity;
 
     @Before("@busca-feature")
     public void setup(){
         activityTestRule.launchActivity(new Intent());
-        setActivity(activityTestRule.getActivity());
+        activity = activityTestRule.getActivity();
     }
 
     @After("@busca-feature")
     public void tearDown() {
-        getAtualActivity().finish();
         activityTestRule.finishActivity();
     }
 
     @Dado("^Eu estou na tela de busca$")
     public void euEstouNaTelaDeBusca(){
-        assertNotNull(getActivity());
+        assertNotNull(activity);
     }
 
     @Quando("^Eu coloco a cidade e não o serviço$")
     public void euColocoACidade(){
-        preencherCampoEditText(R.id.etServico,"");
-        preencherCampoEditText(R.id.etCidade,"Garanhuns");
+        onView(withId(R.id.etServico)).perform(typeText(""));
+        onView(withId(R.id.etCidade)).perform(typeText("Garanhuns"), closeSoftKeyboard());
     }
 
     @Quando("^Eu coloco o serviço e não a cidade$")
     public void euColocoOServico(){
-        preencherCampoEditText(R.id.etCidade,"");
-        preencherCampoEditText(R.id.etServico,"Corte simples");
+        onView(withId(R.id.etCidade)).perform(typeText(""));
+        onView(withId(R.id.etServico)).perform(typeText("Corte simples"), closeSoftKeyboard());
     }
 
     @Quando("^Eu coloco o serviço e a cidade$")
     public void euColocoOsDois(){
-        preencherCampoEditText(R.id.etCidade, "Garanhuns");
-        preencherCampoEditText(R.id.etServico,"Corte simples");
+        onView(withId(R.id.etCidade)).perform(typeText("Garanhuns"));
+        onView(withId(R.id.etServico)).perform(typeText("Corte simples"), closeSoftKeyboard());
     }
 
     @Quando("^Eu clico em buscar sem preencher os campos$")
     public void euColocoNenhum(){
-        preencherCampoEditText(R.id.etCidade, "");
-        preencherCampoEditText(R.id.etServico,"");
+        onView(withId(R.id.etCidade)).perform(typeText(""));
+        onView(withId(R.id.etServico)).perform(typeText(""));
     }
 
     @E("^Eu clico em buscar$")
     public void clicoEmBuscar(){
-        apertarBotao(R.id.btnBuscar);
+        onView(withId(R.id.btnBuscar)).perform(click());
     }
 
     @Entao("^Eu devo ver os salões retornados$")
     public void verRetorno(){
-        verificarMensagemToast(getActivity().getString(R.string.resultados));
+        onView(withText(R.string.resultados)).
+                inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).
+                check(matches(isDisplayed()));
     }
 
     @Entao("^Devo ver uma mensagem dizendo para digitar algum dado$")
     public void verMensagem(){
-        verificarMensagemToast(getActivity().getString(R.string.digite_algum_dado));
+        onView(withText(R.string.digite_algum_dado)).
+                inRoot(withDecorView(not(is(activity.getWindow().getDecorView())))).
+                check(matches(isDisplayed()));
+    }
+
+    /**
+     *
+     * @return referência da activity que está sendo exibida na tela
+     */
+    public Activity getAtualActivity() {
+        final Activity[] activity = new Activity[1];
+        onView(isRoot()).check(new ViewAssertion() {
+            @Override
+            public void check(View view, NoMatchingViewException noViewFoundException) {
+                activity[0] = (Activity) view.getContext();
+            }
+        });
+        return activity[0];
     }
 }
