435eb56b35d12adae4db20d51fd88c103da086b2
RafaelMarquesMiranda <marquesmiranda.r@gmail.com>
2019-01-04 13:36:54 -0300

    classe promocoes atributos ,construtor e metodos, criação da activit de  promoções

---
+++ app/src/main/java/br/com/belapp/belapp/activities/PromocoesActivity.java
@@ -0,0 +1,13 @@
+package br.com.belapp.belapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PromocoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_promocoes);
+    }
+}
--- app/src/main/java/br/com/belapp/belapp/model/Promocoes.java
+++ app/src/main/java/br/com/belapp/belapp/model/Promocoes.java
@@ -3,4 +3,70 @@ package br.com.belapp.belapp.model;
 public class Promocoes {
 
     //Aqui atributos e metodos da classe
+    private String idPromocao;
+    private String estado;
+    private String categoria;
+    private String titulo;
+    private String descricao;
+    private String telefone;
+    private String foto;
+
+    public Promocoes() {
+    }
+
+    public String getIdPromocao() {
+        return idPromocao;
+    }
+
+    public void setIdPromocao(String idPromocao) {
+        this.idPromocao = idPromocao;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
 }
